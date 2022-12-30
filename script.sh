6985e1d2ee0c80fd6fd18c37eefa19829414332c
Subhodeep <subhodeeppal64@gmail.com>
2022-12-30 12:25:59 +0530

    add-sense added

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ docker buildx create --name mybuilder
 docker buildx use mybuilder
 docker buildx inspect --bootstrap
 # docker build and push to docker hub
-docker buildx build --platform linux/amd64 -t subhodeep2022/react-app:weather_fontend --push .
+docker buildx build --platform linux/amd64 -t subhodeep2022/react-app:weather_fontend1 --push .
